248ac967744690759e0b40563de8943b3f06627b
Juliano Ribeiro <julianorib@gmail.com>
2024-07-03 15:57:29 -0300

    meu site novo

--- mysite.py
+++ mysite.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def inicial():
   return render_template("index.html")
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=int("8080"),debug=True)
+  app.run(host="0.0.0.0", port=int("8080"))
