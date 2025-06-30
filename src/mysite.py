d746789b38806aae79e9d0d6482df7c39ed9c86b
Juliano Ribeiro Silva <julianorib@gmail.com>
2025-06-30 20:21:34 -0300

    Page Blue Green

--- src/mysite.py
+++ src/mysite.py
@@ -8,5 +8,13 @@ metrics = PrometheusMetrics(app)
 def inicial():
   return render_template("index.html")
 
+@app.route("/blue")
+def blue():
+  return render_template("blue.html")
+
+@app.route("/green")
+def green():
+  return render_template("green.html")
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=int("8080"))
